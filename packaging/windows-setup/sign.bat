5fcb38b3e92d2ba4b42410aee1c82cc61dd16215
RedLime <redlimerl@gmail.com>
2023-03-21 01:18:48 +0900

    sign installer exe file

---
+++ packaging/windows-setup/sign.bat
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Windows Kits\10\bin\10.0.19041.0\x64\signtool.exe" sign /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /a ./Output/MCSRRankedLauncher-setup-1.2.exe
+pause
